6b9d9133f89c953ac4793a84392babcd61cb17f7
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-11-02 20:34:24 -0300

    Feat: swiper

--- src/components/projects-obj.jsx
+++ src/components/projects-obj.jsx
@@ -1,40 +1,29 @@
-{/* <BodyContainer>
-<Title>Track-It </Title>
-<Subtitle>Aplicativo Web de acompanhamento de hábitos</Subtitle>
-<Gif src={TrackIt}/>
-<a href="https://projeto-11-track-it-pi.vercel.app/">Clique aqui para testar!</a>
-</BodyContainer>
-<BodyContainer>
-<Title>Track-It </Title>
-<Subtitle>Aplicativo de acompanhamento de hábitos</Subtitle>
-<a href="https://projeto-11-track-it-pi.vercel.app/">Clique aqui para testar!</a>
-</BodyContainer>
-<BodyContainer>
-<Title>Track-It </Title>
-<Subtitle>Aplicativo de acompanhamento de hábitos</Subtitle>
-<a href="https://projeto-11-track-it-pi.vercel.app/">Clique aqui para testar!</a>
-</BodyContainer>
-</HomepageContainer> */}
-
-// create an object array that contains title, image, technologies, icon and description
-import TrackIt from "../assets/trackit.gif"
-import Cineflex from "../assets/cineflex.gif"
+import TrackIt from "../assets/trackit.gif";
+import Cineflex from "../assets/cineflex.gif";
+import MyWallet from "../assets/mywallet.gif";
 import Html from "../assets/html.png"
 import Css from "../assets/css.png"
 import ReactIcon from "../assets/react.png"
 import JavaScript from "../assets/javascript.png"
+import MongoDB from "../assets/mongodb.png"
 
 export const projectsObject = [
+    {
+        title: "MyWallet",
+        image: MyWallet,
+        technologies: "React, MongoDB, JavaScript, HTML, CSS",
+        icon: [
+            ReactIcon, MongoDB, JavaScript, Html, Css          
+        ], 
+        description: "Aplicativo Web para controle de finanças"
+    },
     {
         title: "Track-It",
         image: TrackIt,
         technologies: "React, JavaScript, HTML, CSS",
         icon: [
-            "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-            "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png",
-            "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
-        ],
+            ReactIcon, JavaScript, Html, Css          
+        ], 
         description: "Aplicativo Web de acompanhamento de hábitos"
     },
     {
@@ -42,10 +31,7 @@ export const projectsObject = [
         image: Cineflex,
         technologies: "React, JavaScript, HTML, CSS",
         icon: [
-            "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-            "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png",
-            "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+            ReactIcon, JavaScript, Html, Css          
         ], 
         description: "Aplicativo Web para reservas de assento de cinema"
     },
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,25 +1,60 @@
 import styled from "styled-components";
 import Header from "../components/header";
 import Card from "../components/card";
-import {projectsObject} from "../components/projects-obj.jsx"
+import {projectsObject} from "../components/projects-obj.jsx";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import '../components/swiper.css';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
 
 export default function Homepage() {
     return (
       <HomepageContainer>
         <Header />
-        {projectsObject.map((project, index) => (
-          <Card
-            key={index}
-            image={project.image}
-            title={project.title}
-            description={project.description}
-            technologies={project.technologies}
-            icon={project.icon}
-          />
-        ))}
+        {/* <CardsContainer>
+          {projectsObject.map((project, index) => (
+            <Card
+              key={index}
+              image={project.image}
+              title={project.title}
+              description={project.description}
+              technologies={project.technologies}
+              icon={project.icon}
+            />
+          ))}
+        </CardsContainer> */}
+        <Swiper 
+          slidesPerView={1}
+          navigation={true} 
+          modules={[Navigation, Pagination] }
+          className="swiper"
+        >
+          {projectsObject.map((project, index) => (
+            <SwiperSlide>
+              <Card
+                key={index}
+                image={project.image}
+                title={project.title}
+                description={project.description}
+                technologies={project.technologies}
+                icon={project.icon}
+              />
+            </SwiperSlide>
+          ))}
+
+
+        </Swiper>
       </HomepageContainer>
     );
 }
+
+const CardsContainer = styled.div`
+  display: flex;
+
+`
 
 const HomepageContainer = styled.div`
     display: flex;
